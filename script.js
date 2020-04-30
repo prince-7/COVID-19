bb52c449e73937a6125d3577234d19241b6543fc
prince-7 <amanvijaykumarsingh7792@gmail.com>
2020-04-30 11:46:31 +0530

    firstDraft

---
+++ script.js
@@ -0,0 +1,11 @@
+function getData()
+{
+	var x = document.getElementById("myDIV");
+
+    x.style.display = "block";
+
+	var country=document.getElementByClassName("push").country.value;
+
+	document.getElementById("myDIV").counttry.innerText="India";
+
+}
